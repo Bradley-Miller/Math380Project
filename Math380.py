f018940afd67a19ad7628b6c635970a9ab582662
Bradley Miller <bradley@localhost.localdomain>
2023-05-02 11:28:15 -0500

    added ida stuff

--- Math380.py
+++ Math380.py
@@ -1,5 +1,5 @@
 import csv
-
+import math
 #Math 380 project, trying to give a probability of damage of a hurricane based on the HSI scale
 
 model_city_pop = 100000 
@@ -30,11 +30,11 @@ with open('Hurricane_Data.csv') as csv_file:
     print(historic_economic_damages)
 
 #read in the HSI index values
-with open('HSI_Data.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter = ',')
-    line_count = 0
-    for row in csv_reader:
-        hsi_index_values.append 
+#with open('HSI_Data.csv') as csv_file:
+#    csv_reader = csv.reader(csv_file, delimiter = ',')
+#   line_count = 0
+#  for row in csv_reader:
+#    hsi_index_values.append 
 
 #read in the hurricane radius data from the NHC
 with open('NHC_Data.csv') as csv_file:
@@ -42,12 +42,30 @@ with open('NHC_Data.csv') as csv_file:
     #start the line count at the first hurricane that has the data for the radius
     line_count = 0
     for row in csv_reader:
-        hurricane_data_NW.append()
-        hurricane_data_NE.append()
-        hurricane_data_SW.append()
-        hurricane_data_SE.append()
+        if(line_count!=0):
+            hurricane_data_NW.append(row[1])
+            hurricane_data_NE.append(row[2])
+            hurricane_data_SW.append(row[4])
+            hurricane_data_SE.append(row[3])
+        line_count+=1
 
 #try to get a formula that will be used to estimate 1 point of damage
+#NW=
+#SW=hurricane_data_
+#NE=hurricane_data_NE[0]
+#SE=hurricane_data_SE[0]
+#print(hurricane_data_NW[0])
+#print(
+effective_radius = (1/2) * math.sqrt(pow(60, 2) + pow(80, 2) + pow(60, 2) + pow(0, 2))
+
+ida_points = 23
+ida_damage = 75000000000
+ida_impact = 57565950
+total_damage = (ida_damage/ida_impact)*model_city_pop/ida_points
+
+print(effective_radius)
+
+print(total_damage)
 
 
 #try to get a formula that will estimate the economic impact of a hurricane on the model city
