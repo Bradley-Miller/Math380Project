58010ec0565c20ea231d7f8de5ba3e3153604935
Bradley Miller <bradley@localhost.localdomain>
2023-05-03 10:48:22 -0500

    changed scope

--- Math380.py
+++ Math380.py
@@ -56,17 +56,88 @@ with open('NHC_Data.csv') as csv_file:
 #SE=hurricane_data_SE[0]
 #print(hurricane_data_NW[0])
 #print(
-effective_radius = (1/2) * math.sqrt(pow(60, 2) + pow(80, 2) + pow(60, 2) + pow(0, 2))
 
-ida_points = 23
-ida_damage = 75000000000
+windSpeed = 105
+vMax = 0
+if(windSpeed <30):
+    vMax = 0
+elif(windSpeed>=30 and windSpeed<=150):
+     vMax = round(pow(windSpeed/30, 2))
+else:
+     vMax = 25
+total_radii_points=0
+
+effective_radius_32 = (1/2) * math.sqrt(pow(110, 2) + pow(175, 2) + pow(90, 2) + pow(75, 2))
+effective_radius_50 = (1/2) * math.sqrt(pow(65, 2) + pow(60, 2) + pow(30, 2) + pow(30, 2))
+effective_radius_65 = (1/2) * math.sqrt(pow(20, 2) + pow(20, 2) + pow(15, 2) + pow(20, 2))
+effective_radius_87 = 0.5683 * effective_radius_65 + (0.0792 * windSpeed) - 9.5393
+
+#assign points based on effective_radii
+if(effective_radius_32<100):
+    total_radii_points+=1
+elif(effective_radius_32<155):
+    total_radii_points+=2
+else:
+    total_radii_points+=3
+
+if(effective_radius_50<50):
+    total_radii_points+=1
+elif(effective_radius_50<75):
+    total_radii_points+=2
+elif(effective_radius_50<105):
+    total_radii_points+=3
+else:
+    total_radii_points+=4
+
+if(effective_radius_65<25):
+    total_radii_points+=1
+elif(effective_radius_65<34):
+    total_radii_points+=2
+elif(effective_radius_65<42):
+    total_radii_points+=3
+elif(effective_radius_65<50):
+    total_radii_points+=4
+elif(effective_radius_65<58):
+    total_radii_points+=5
+elif(effective_radius_65<66):
+    total_radii_points+=6
+elif(effective_radius_65<74):
+    total_radii_points+=7
+else:
+    total_radii_points+=8
+
+if(effective_radius_87<13):
+    total_radii_points+=1
+elif(effective_radius_87<17):
+    total_radii_points+=2
+elif(effective_radius_87<22):
+    total_radii_points+=3
+elif(effective_radius_87<24):
+    total_radii_points+=4
+elif(effective_radius_87<30):
+    total_radii_points+=5
+elif(effective_radius_87<34):
+    total_radii_points+=6
+elif(effective_radius_87<38):
+    total_radii_points+=7
+elif(effective_radius_87<42):
+    total_radii_points+=8
+elif(effective_radius_87<48):
+    total_radii_points+=9
+else:
+    total_radii_points+=10
+
+total_points = vMax + total_radii_points
+
+ida_points = 18
+ida_damage = 2200000000
 ida_impact = 57565950
-total_damage = (ida_damage/ida_impact)*model_city_pop/ida_points
+total_damage = (ida_damage/ida_points)
 
 print(effective_radius)
 
 print(total_damage)
-
+print(total_points)
 
 #try to get a formula that will estimate the economic impact of a hurricane on the model city
 
